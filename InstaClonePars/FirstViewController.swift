5080dba86c91d80121048a9d6c86fcf9859f526d
furkan elkatmış <furkanelkatmis@furkan-macbook-pro.local>
2019-01-06 02:28:53 +0300

    Initial Commit

---
+++ InstaClonePars/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  InstaClonePars
+//
+//  Created by furkan elkatmış on 6.01.2019.
+//  Copyright © 2019 furkan elkatmış. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ InstaClonePars/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InstaClonePars
+//
+//  Created by furkan elkatmış on 6.01.2019.
+//  Copyright © 2019 furkan elkatmış. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
